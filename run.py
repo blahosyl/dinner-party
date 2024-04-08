2d8b280216e7d3d60ae87d2e2ab2dc00af79b53d
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-04-08 02:17:52 +0200

    make `get_ingredients()` return a list of lists #5

--- run.py
+++ run.py
@@ -27,20 +27,29 @@ print(data)
 # subsequent items of each row are the quantity of ingredients needed for the corresponding dish
 # when an ingredient is not needed for a dish, the corresponding cell is empty
 
+# create empty shopping list (will be a list of lists)
+shopping_list = []
 
 # select a dish – this will be done by user input in the final version
 selected_dish = data[1][4]
-print(selected_dish)
 
 # Get list of ingredients for a selected dish
 def get_ingredients(selection):
+    print(selected_dish)
+    # empty list of ingredients
+    ingredient_list = []
     # get the index of the selected dish
     selection_index = data[1].index(selection)
     # go through each row
     for row in data:
         # if the cell in the row under the selected dish has content (ingredient quantity)
-        if row[selection_index]:
+        if row[selection_index] and data.index(row) > 1:
             # print the ingredient name and amount
             print(row[0]+' '+row[selection_index])
+            ingredient_list.append([row[0],row[selection_index]])
+    return ingredient_list
 
-get_ingredients(selected_dish)
+shopping_list.append(get_ingredients(selected_dish))
+
+print(shopping_list)
+
