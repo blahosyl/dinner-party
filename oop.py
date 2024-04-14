807574ada36cb0539cd9c50c1e452025367efeae
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-04-14 15:52:16 +0200

    add `select_dishes()` to `DishList

--- oop.py
+++ oop.py
@@ -15,7 +15,39 @@ class DishList:
         # print an empty line to visually separate the list
         print('\n')
 
+    def select_dish(self):
+        # print the list of dishes in a numbered list
+        self.print_enum()
+
+        # validating the input
+        while True:
+            try:
+                # ask user to select a dish number
+                dish_number = int(input("Type in the number of the dish you'd like to add: "))
+                while dish_number < 1 or dish_number > len(self.dish_data) - 1:
+                    dish_number = int(
+                        input(f"Number out of range. Please type a number between 1 and {len(self.dish_data) - 1}: "))
+                break
+            except ValueError:
+                # while the input is not one of the allowed options
+                # ask for input again
+                print(f"That is not a valid number. Please type a whole number between 1 and {len(self.dish_data) - 1}.")
+
+        # get the dish with the selected number from the `dishes` list
+        selected_dish = self.dish_data[int(dish_number)]
+
+        # print the selected dish's name
+        print(f'\n You have selected: {selected_dish}')
+
+        # remove the selected dish from the list of available dishes
+        self.dish_data.remove(selected_dish)
+
+        return selected_dish
+
 
 dishes = DishList(["biscuits", "tea", "dip"])
 
+dishes.select_dish()
+
+# check if the selected dish has been removed from the list
 dishes.print_enum()
